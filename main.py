233977ff1e1769e05ab86f14d64650a21a9ebfb6
Matthew <matthewj.oconnor01@gmail.com>
2026-03-24 16:11:41 +0000

    Returns pandas df containing unverified sheep

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('data/data.csv')
 
-print(df)
+df.columns = [column.replace(" ", "_") for column in df.columns]
+
+df['Pedigree_Status'] = df['Pedigree_Status'].str.replace(" ", "_")
+
+print(df.query("Pedigree_Status == 'Birth_Notified'"))
